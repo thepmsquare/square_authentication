5fb6e9b6a5491553634b65ce4f2289459198e017
thepmsquare <impmmmm@gmail.com>
2025-04-03 09:48:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = "square_authentication"
 
 setup(
     name=package_name,
-    version="5.1.2",
+    version="5.1.3",
     packages=find_packages(),
     package_data={
         package_name: ["data/*"],
